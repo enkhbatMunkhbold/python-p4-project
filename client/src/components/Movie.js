c7a144dc840d184f3aa5feda52f9a2147a01f917
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2025-04-19 10:50:02 -0700

    Add number and time selection onto Movie component

--- client/src/components/Movie.js
+++ client/src/components/Movie.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import "../styling/Movie.css"
 
 const Movie = ({ movie }) => {
   const navigate = useNavigate()
+  const [ selectedNumber, setSelectedNumber ] = useState(1)
+  const [ selectedTime, setSelectedTime ] = useState('12:00 pm')
+
+  const time = ['12:00 pm', '2:00 pm', '4:00 pm', '6:00 pm', '8:00 pm']
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
   const handleSelect = () => {
     navigate("/profile", { state: { movieId: movie.id } })
@@ -16,6 +21,26 @@ const Movie = ({ movie }) => {
     <div className='movie-item'>
       <div className='movie-content'>
         <h3>{movie.title}</h3>
+        <div className='selection-controls'>
+          <select
+            value={selectedNumber}
+            onChange={(e) => setSelectedNumber(e.target.value)}
+            className='ticket-selection'
+          >
+            {numbers.map(num => (
+              <option key={num} value={num}>
+                {num} ticket{num > 1 ? 's' : ''}
+              </option>
+            ))}
+          </select>
+          <select
+            value={selectedTime}
+            onChange={(e) => setSelectedTime(e.target.value)}
+            className='time-selection'
+          >
+            {time.map(t => (<option key={t} value={t}>{t}</option>))}
+          </select>
+        </div>
         <button onClick={handleSelect}>
           SELECT
         </button>  
